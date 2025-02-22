6773014c593ba37409a057ea6ed4b523f96e7365
Pavel Maksimovich <diogoc1707@gmail.com>
2025-02-22 14:30:38 +0500

    disable supermaven

--- lua/plugins/coding/blink.lua
+++ lua/plugins/coding/blink.lua
@@ -82,14 +82,14 @@ return {
             enabled = true,
         },
         sources = {
-            compat = { "supermaven" },
+            compat = {},
             default = { "lsp", "path", "snippets", "buffer", "lazydev" },
             providers = {
-                supermaven = {
-                    kind = "Supermaven",
-                    score_offset = 100,
-                    async = true,
-                },
+                -- supermaven = {
+                --     kind = "Supermaven",
+                --     score_offset = 100,
+                --     async = true,
+                -- },
                 lazydev = {
                     name = "LazyDev",
                     module = "lazydev.integrations.blink",
